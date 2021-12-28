f27660af7004d027c7167b1ec6b9bc2f878396b8
nxbeyxnd <unblocksteam@gmail.com>
2021-12-28 23:15:00 +0300

    Added 2 local var to MyArrayList

--- src/main/java/ru/andersen/listImpl/Main.java
+++ src/main/java/ru/andersen/listImpl/Main.java
@@ -3,6 +3,11 @@ package ru.andersen.listImpl;
 28.12.2021: Alexey created this file inside the package: ru.andersen.listImpl 
 */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+    }
 }
--- src/main/java/ru/andersen/listImpl/MyArrayList.java
+++ src/main/java/ru/andersen/listImpl/MyArrayList.java
@@ -3,5 +3,7 @@ package ru.andersen.listImpl;
 28.12.2021: Alexey created this file inside the package: ru.andersen.listImpl 
 */
 
-public class MyArrayList {
+public class MyArrayList<E> {
+    private final int INITIAL_CAPACITY = 5;
+    private final int size = 0;
 }
