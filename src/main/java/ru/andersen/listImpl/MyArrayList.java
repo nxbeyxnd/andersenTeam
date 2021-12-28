16c7e0e125d9ca28c6ab3301cf011df35f289226
nxbeyxnd <unblocksteam@gmail.com>
2021-12-28 23:17:12 +0300

    fix vars

--- src/main/java/ru/andersen/listImpl/MyArrayList.java
+++ src/main/java/ru/andersen/listImpl/MyArrayList.java
@@ -5,5 +5,7 @@ package ru.andersen.listImpl;
 
 public class MyArrayList<E> {
     private final int INITIAL_CAPACITY = 5;
-    private final int size = 0;
+    private final Object[] LOCALARRAY = {};
+
+    private int size = 0;
 }
