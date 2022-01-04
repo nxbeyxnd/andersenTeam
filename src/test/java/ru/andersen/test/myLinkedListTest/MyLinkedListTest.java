262a5696786059746832d70a907364fd854d0716
nxbeyxnd <unblocksteam@gmail.com>
2022-01-04 23:58:19 +0300

    fix names

--- src/test/java/ru/andersen/test/myLinkedListTest/MyLinkedListTest.java
+++
@@ -1,7 +0,0 @@
-package ru.andersen.test.myLinkedListTest;
-/* 
-04.01.2022: Alexey created this file inside the package: ru.andersen.test.myLinkedListTest 
-*/
-
-public class MyLinkedListTest {
-}
