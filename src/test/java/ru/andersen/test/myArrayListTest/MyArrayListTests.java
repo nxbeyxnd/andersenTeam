6e6c0b4a2fa0049c5118d0211749a6c87251d8b2
nxbeyxnd <unblocksteam@gmail.com>
2022-01-09 18:11:23 +0300

    Fix arrayList sort

--- src/test/java/ru/andersen/test/myArrayListTest/MyArrayListTests.java
+++ src/test/java/ru/andersen/test/myArrayListTest/MyArrayListTests.java
@@ -3,13 +3,12 @@ package ru.andersen.test.myArrayListTest;/*
 */
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-
 import ru.andersen.app.my_array_list_impl.MyArrayList;
 import ru.andersen.app.my_array_list_impl.MyArrayListImpl;
 import ru.andersen.app.my_array_list_impl.MyClass;
+
+import java.util.Comparator;
 
 
 public class MyArrayListTests {
@@ -22,60 +21,71 @@ public class MyArrayListTests {
 
 
     @Test
-    public void addNextElement(){
+    public void addNextElement() {
         list.add(one);
         list.add(two);
         list.add(third);
-        Assertions.assertEquals(list.get(1),two);
+        Assertions.assertEquals(list.get(1), two);
     }
+
     @Test
-    public void addElementByIndex(){
+    public void addElementByIndex() {
         list.add(two);
         list.add(third);
-        list.add(1,one);
-        Assertions.assertEquals(list.get(1),one);
+        list.add(1, one);
+        Assertions.assertEquals(list.get(1), one);
     }
+
     @Test
-    public void setElementByIndex(){
+    public void setElementByIndex() {
         list.add(one);
         list.add(two);
-        list.set(1,third);
-        Assertions.assertEquals(list.get(1),third);
+        list.set(1, third);
+        Assertions.assertEquals(list.get(1), third);
     }
+
     @Test
-    public void sort(){
+    public void sort() {
         list.add(one);
         list.add(two);
         list.add(third);
-        list.sort();
+        list.sort(new Comparator<MyClass>() {
+            @Override
+            public int compare(MyClass o1, MyClass o2) {
+                return o1.compareTo(o2);
+            }
+        });
         MyArrayList expected = new MyArrayListImpl();
         expected.add(third);
         expected.add(one);
         expected.add(two);
-        Assertions.assertEquals(expected.toString(),list.toString());
+        Assertions.assertEquals(expected.toString(), list.toString());
     }
+
     @Test
-    public void removeByElement(){
+    public void removeByElement() {
         list.add(one);
         list.add(two);
         list.add(third);
         list.remove(list.get(1));
-        Assertions.assertEquals(list.get(1),third);
+        Assertions.assertEquals(list.get(1), third);
     }
+
     @Test
-    public void removeByindex(){
+    public void removeByindex() {
         list.add(one);
         list.add(two);
         list.add(third);
         list.remove(1);
-        Assertions.assertEquals(list.get(1),third);
+        Assertions.assertEquals(list.get(1), third);
     }
+
     @Test
-    public void contains(){
+    public void contains() {
         list.add(one);
         list.add(two);
         list.add(third);
         list.contains(third);
-        Assertions.assertEquals(true,true);
+        Assertions.assertEquals(true, true);
     }
 }
