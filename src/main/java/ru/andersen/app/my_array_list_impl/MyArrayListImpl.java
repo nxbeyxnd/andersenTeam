979c3157a24451adead6fcb32d0d5e6db1ced296
nxbeyxnd <unblocksteam@gmail.com>
2022-01-09 18:11:23 +0300

    Fix arrayList sort

--- src/main/java/ru/andersen/app/my_array_list_impl/MyArrayListImpl.java
+++ src/main/java/ru/andersen/app/my_array_list_impl/MyArrayListImpl.java
@@ -6,16 +6,16 @@ package ru.andersen.app.my_array_list_impl;
 import java.util.Arrays;
 import java.util.Comparator;
 
-public class MyArrayListImpl<E extends MyClass> implements MyArrayList, Comparator<MyClass> {
+public class MyArrayListImpl<E> implements MyArrayList<E> {
     private final Object[] DEFAULT_LOCAL_ARRAY = {};
-    private Object[] localArray;
+    private E[] localArray;
     private int size = 0;
 
     /**
      * Пустой конструктор
      */
     public MyArrayListImpl() {
-        this.localArray = DEFAULT_LOCAL_ARRAY;
+        this.localArray = (E[]) DEFAULT_LOCAL_ARRAY;
     }
 
     /**
@@ -24,45 +24,45 @@ public class MyArrayListImpl<E extends MyClass> implements MyArrayList, Comparat
      * @param capacity заданная длинна массива пользователем
      */
     public MyArrayListImpl(int capacity) {
-        if (capacity > 0) this.localArray = new Object[capacity];
-        else if (capacity == 0) this.localArray = DEFAULT_LOCAL_ARRAY;
+        if (capacity > 0) this.localArray = (E[]) new Object[capacity];
+        else if (capacity == 0) this.localArray = (E[]) DEFAULT_LOCAL_ARRAY;
         else throw new IllegalArgumentException("Некорректный аргумент: " + capacity);
     }
 
     /**
      * Добавление объекта с динамическим увеличением длинны массива
-     * @param o объект пользователя
+     * @param e объект пользователя
      */
     @Override
-    public void add(Object o) {
+    public void add(E e) {
         if (this.size == this.localArray.length) {
             this.changeSize(++size);
-            add(o, this.localArray, size - 1);
+            add(e, this.localArray, size - 1);
         }
     }
 
     /**
      * Добавление объекта по индексу с динамическим увеличением длинны массива
      * @param index индекс в массиве
-     * @param o объект пользователя
+     * @param e объект пользователя
      */
     @Override
-    public void add(int index, Object o) {
+    public void add(int index, E e) {
         Object[] temp = this.localArray;
         changeSize(++size);
         System.arraycopy(temp, index - 1, this.localArray, index, size - index);
-        localArray[index] = o;
+        localArray[index] = e;
     }
 
     /**
      * Добавление объекта в уже увеличенный по длине массив
      *
-     * @param o          Объект пользователя
+     * @param e          Объект пользователя
      * @param localArray Текущий массив
      * @param index      индекс в массиве
      */
-    private void add(Object o, Object[] localArray, int index) {
-        localArray[index] = o;
+    private void add(E e, E[] localArray, int index) {
+        localArray[index] = e;
     }
 
     /**
@@ -71,19 +71,19 @@ public class MyArrayListImpl<E extends MyClass> implements MyArrayList, Comparat
      * @return объект в массиве
      */
     @Override
-    public Object get(int index) {
+    public E get(int index) {
         return this.localArray[index];
     }
 
     /**
      * Проверка на существование объекта в массиве
-     * @param o Искомый объект
+     * @param e Искомый объект
      * @return True - в случае совпадения, False - в случае отсутствия
      */
     @Override
-    public boolean contains(Object o) {
+    public boolean contains(E e) {
         for (int i = 0; i < localArray.length; i++) {
-            if (o.equals(localArray[i])) {
+            if (e.equals(localArray[i])) {
                 return Boolean.TRUE;
             }
         }
@@ -93,11 +93,11 @@ public class MyArrayListImpl<E extends MyClass> implements MyArrayList, Comparat
     /**
      * Замена объекта по индексу
      * @param index индекс объекта
-     * @param o объект пользователя
+     * @param e объект пользователя
      */
     @Override
-    public void set(int index, Object o) {
-        this.localArray[index] = o;
+    public void set(int index, E e) {
+        this.localArray[index] = e;
     }
 
     /**
@@ -107,7 +107,7 @@ public class MyArrayListImpl<E extends MyClass> implements MyArrayList, Comparat
      */
     @Override
     public void remove(int index) {
-        Object[] temp = this.localArray;
+        E[] temp = this.localArray;
         changeSize(--size);
         System.arraycopy(temp, 0, localArray, 0, size);
         System.arraycopy(temp, index + 1, localArray, index, size - index);
@@ -115,12 +115,12 @@ public class MyArrayListImpl<E extends MyClass> implements MyArrayList, Comparat
 
     /**
      * Удаление объекта в массиве, если он там находится
-     * @param o объект
+     * @param e объект
      */
     @Override
-    public void remove(Object o) {
+    public void remove(E e) {
         for (int i = 0; i < localArray.length; i++) {
-            if (o.equals(localArray[i])) {
+            if (e.equals(localArray[i])) {
                 remove(i);
             }
         }
@@ -136,39 +136,14 @@ public class MyArrayListImpl<E extends MyClass> implements MyArrayList, Comparat
     }
 
     /**
-     * Сортировка массива по Имени и Возрасту объектов
+     * Сортировка массива по компаратору
+     * @param c Comparator<E> объекта сравнения
      */
     @Override
-    public void sort() {
+    public void sort(Comparator<E> c) {
         for (int k = size; k != 0; k--) {
             for (int i = 0, j = 1; j < size; i++, j++) {
-                if (compare((MyClass) localArray[i], (MyClass) localArray[j]) > 0) {
-                    swap(i, j);
-                }
-            }
-        }
-    }
-
-    /**
-     * Сортировка массива по Имени объектов
-     */
-    public void sortByName() {
-        for (int k = size; k != 0; k--) {
-            for (int i = 0, j = 1; j < size; i++, j++) {
-                if (compareByName((MyClass) localArray[i], (MyClass) localArray[j]) > 0) {
-                    swap(i, j);
-                }
-            }
-        }
-    }
-
-    /**
-     * Сортировка массива по Возрасту объектов
-     */
-    public void sortByAge() {
-        for (int k = size; k != 0; k--) {
-            for (int i = 0, j = 1; j < size; i++, j++) {
-                if (compareByAge((MyClass) localArray[i], (MyClass) localArray[j]) > 0) {
+                if (c.compare(localArray[i], localArray[j]) > 0) {
                     swap(i, j);
                 }
             }
@@ -181,7 +156,7 @@ public class MyArrayListImpl<E extends MyClass> implements MyArrayList, Comparat
      * @param j индекс 2 элемента
      */
     private void swap(int i, int j) {
-        Object temp = localArray[i];
+        E temp = localArray[i];
         localArray[i] = localArray[j];
         localArray[j] = temp;
     }
@@ -193,37 +168,6 @@ public class MyArrayListImpl<E extends MyClass> implements MyArrayList, Comparat
      */
     private void changeSize(int newSize) {
         this.localArray = Arrays.copyOf(this.localArray, newSize);
-    }
-
-    /**
-     * Сравнение двух объектов по Name и Age
-     * @param o1 1й объект
-     * @param o2 2й объект
-     * @return положительное в случае если o1 > o2, 0 - объекты равны, отрицательное если o1 < 02
-     */
-    @Override
-    public int compare(MyClass o1, MyClass o2) {
-        return o1.compareTo(o2);
-    }
-
-    /**
-     * Сравнение двух объектов по Name
-     * @param o1 1й объект
-     * @param o2 2й объект
-     * @return положительное в случае если o1 > o2, 0 - объекты равны, отрицательное если o1 < 02
-     */
-    public int compareByName(MyClass o1, MyClass o2) {
-        return o1.getName().compareTo(o2.getName());
-    }
-
-    /**
-     * Сравнение двух объектов по Age
-     * @param o1 1й объект
-     * @param o2 2й объект
-     * @return положительное в случае если o1 > o2, 0 - объекты равны, отрицательное если o1 < 02
-     */
-    public int compareByAge(MyClass o1, MyClass o2) {
-        return Integer.compare(o1.getAge(), o2.getAge());
     }
 
     @Override
