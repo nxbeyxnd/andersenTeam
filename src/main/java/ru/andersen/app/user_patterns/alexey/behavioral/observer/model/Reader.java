7899036ea5b396c3105a6c9dfdf4e2438c3dde2a
Alexey <unblocksteam@gmail.com>
2022-01-18 18:08:00 +0300

    Dev (#27)
    * Refactoring some classes
    
    * Refactoring some classes
    
    * Added Bridge pattern
    
    * Added facade pattern
    
    * Added decorator pattern
    
    * Fixed Composite pattern
    
    * added Proxy pattern
    
    * added flyweight pattern
    
    * added COR pattern
    
    * added Command pattern
    
    * added Visitor pattern
    
    * added Observer pattern

---
+++ src/main/java/ru/andersen/app/user_patterns/alexey/behavioral/observer/model/Reader.java
@@ -0,0 +1,19 @@
+package ru.andersen.app.user_patterns.alexey.behavioral.observer.model;
+/* 
+18.01.2022: Alexey created this file inside the package: ru.andersen.app.user_patterns.alexey.behavioral.observer.model 
+*/
+
+import java.util.List;
+
+public class Reader implements Observer {
+    String name;
+
+    public Reader(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void handleEvent(List<String> posts) {
+        System.out.println("updating " + name + " page..." + posts);
+    }
+}
