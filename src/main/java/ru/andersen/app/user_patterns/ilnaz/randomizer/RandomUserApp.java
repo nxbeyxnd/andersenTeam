fc330d87d16210002b8dfa2db28395628bb51806
nxbeyxnd <unblocksteam@gmail.com>
2022-01-20 21:43:46 +0300

    Random task solved.

---
+++ src/main/java/ru/andersen/app/user_patterns/ilnaz/randomizer/RandomUserApp.java
@@ -0,0 +1,58 @@
+package ru.andersen.app.user_patterns.ilnaz.randomizer;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static ru.andersen.app.user_patterns.ilnaz.randomizer.ListOfStudents.students;
+
+public class RandomUserApp {
+    Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
+
+    private Student randomStudentAskQuestion() {
+        List<Student> s = students.stream()
+                .filter(x -> !x.isQuestion())
+                .collect(Collectors.toList());
+        if (s.isEmpty()) return null;
+        return s.get(random.nextInt(s.size()));
+    }
+
+    private Student randomStudentAnswers(Student first, Student student) {
+        List<Student> s = students.stream()
+                .filter(x -> !x.equals(student)
+                        && !x.equals(first)
+                        && !x.isAnswer()
+                        && x.getTeamId() != student.getTeamId())
+                .collect(Collectors.toList());
+        if (s.isEmpty()) return null;
+        return s.get(random.nextInt(s.size()));
+    }
+
+    private void printResult(Student s1, Student s2) {
+        System.out.println("Вопрос задает: "
+                + s1.getName().toUpperCase(Locale.ROOT) + " команда № " + s1.getTeamId() +
+                ".\nОтвечает: " + s2.getName().toUpperCase(Locale.ROOT) + " команда № " + s2.getTeamId() + ".");
+
+    }
+
+    public void coupleRandomStudent() {
+        Student studentQuestion = randomStudentAskQuestion();
+        Student first = studentQuestion;
+        while (true) {
+            Student studentAnswer = randomStudentAnswers(first, studentQuestion);
+            if (studentAnswer == null) {
+                studentAnswer = first;
+                printResult(studentQuestion, studentAnswer);
+                break;
+            }
+            studentQuestion.setQuestion(true);
+            studentAnswer.setAnswer(true);
+            printResult(studentQuestion, studentAnswer);
+            studentQuestion = studentAnswer;
+            scanner.nextLine();
+        }
+    }
+}
--- src/main/java/ru/andersen/app/user_patterns/ilnaz/randomizer/Runner.java
+++ src/main/java/ru/andersen/app/user_patterns/ilnaz/randomizer/Runner.java
@@ -1,17 +1,9 @@
 package ru.andersen.app.user_patterns.ilnaz.randomizer;
 
-import java.util.Scanner;
-import static ru.andersen.app.user_patterns.ilnaz.randomizer.ListOfStudents.students;
-
 public class Runner {
     public static void main(String[] args) {
-        Random random = new Random();
+        RandomUserApp randomUserApp = new RandomUserApp();
 
-           random.coupleRandomStudent();
-
-
-
-        System.out.println("Опрос завершен");
-        System.out.println(students);
+            randomUserApp.coupleRandomStudent();
     }
 }
